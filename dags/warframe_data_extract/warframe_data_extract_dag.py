2a04174add25e926abb5a0d6fc1ef825e0fc831e
William Harris <william.michael.h@icloud.com>
2024-12-17 19:16:21 -0600

    Added warframe_data_extract dag

---
+++ dags/warframe_data_extract/warframe_data_extract_dag.py
@@ -0,0 +1,24 @@
+import datetime
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
+
+with DAG(
+    dag_id="",
+    schedule_interval="0 * * * *",
+    start_date=datetime.datetime(2024, 12, 17),
+    catchup=False
+) as dag:
+
+    t0a = EmptyOperator(task_id="start")
+
+    t1 = EmptyOperator(task_id="load_data")
+
+    t2 = EmptyOperator(task_id="transform_data")
+
+    t3 = EmptyOperator(task_id="upload_data")
+
+    t0b = EmptyOperator(task_id="end")
+
+    t0a >> t1 >> t2 >> t3 >> t0b
