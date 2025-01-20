6a8d01fac10a42fa7f6934625a8f537be274852a
William Harris <william.michael.h@icloud.com>
2025-01-19 19:28:03 -0600

    Moved warframe object to DAG directly due to import difficulties

--- dags/warframe_data_extract/warframe_data_extract_dag.py
+++ dags/warframe_data_extract/warframe_data_extract_dag.py
@@ -9,113 +9,131 @@ from airflow.models.variable import Variable
 
 DAG_ID = "warframe_data_extract"
 
-from dataclasses import dataclass, field
-from bs4 import Tag, PageElement
+from dataclasses import dataclass
+from bs4 import BeautifulSoup
+import requests
 import re
 
 
 @dataclass
-class Reward:
-    """A reward on a drop table"""
-    name: str
-    rarity: str
+class Chance:
+    description: str
     percentage: float
 
-    @classmethod
-    def from_html_table_row(cls, tr: Tag):
-        properties_re = re.compile(r"(?P<rarity>.*)\s\((?P<percentage>\d{1,3}\.?\d{,2})%\)")
-        name, properties = tr.select("td")
-        name = name.text.strip()
-        properties = properties_re.match(properties.text).groupdict()
-        return cls(
-            name=name,
-            rarity=properties["rarity"].strip(),
-            percentage=float(properties["percentage"])
-        )
-
-    def __str__(self):
-        return f"{self.name} - {self.rarity} ({self.percentage:.2f}%)"
-
 @dataclass
-class Table:
+class Drop:
     name: str
-    rewards: list[Reward]
-
-    @classmethod
-    def from_list_of_tags(cls, tag_list: list[Tag]):
-        pass
-
+    chance: Chance
 
 @dataclass
-class Rotation(Table):
-    @classmethod
-    def from_list_of_tags(cls, tag_list: list[Tag]):
-        header = tag_list[0]
-        tag_list = tag_list[1:]
-        return cls(
-            name=header.text.strip(),
-            rewards=[Reward.from_html_table_row(t) for t in tag_list]
-        )
-
+class Stage:
+    primary: int
+    group: int = None
+    # Stage 1, Stage 2, Stage 3 of 4, Stage 3 of 5, etc
 
 @dataclass
-class DropTable(Table):
-    """For tracking a drop table from the official Warframe Drop tables"""
-    rotations: list[Rotation] = field(default_factory=list)
-    rewards: list[Reward] = field(default_factory=list)
-
-    @classmethod
-    def from_list_of_tags(cls, tag_list: list[Tag]):
-        rewards = []
-        rotations = []
-        # First tag is the header
-        name = tag_list[0].text.strip()
-        tag_list = tag_list[1:]
-        if "rotation" in tag_list[0].text.strip().lower():
-            # We have rotations
-            rotation_list = [tag_list[0]]
-            for tag in tag_list[1:]:
-                if "rotation" in tag.text.strip().lower():
-                    if rotation_list:
-                        # We have a list of rotation items, thus we've hit a new rotation
-                        rotations.append(Rotation.from_list_of_tags(rotation_list))
-                        rotation_list = [tag]
-                        continue
-                rotation_list.append(tag)
-        else:
-            # We have a direct rewards list
-            rewards = [Reward.from_html_table_row(t) for t in tag_list]
-
-        return cls(
-            name=name,
-            rotations=rotations,
-            rewards=rewards
-        )
-
-
-@dataclass
-class DropTableType:
-    """A collection of related drop tables (relics, missions, etc)"""
+class Selector:
     name: str
-    drop_tables: list[DropTable] = field(default_factory=list)
+    rotation: str
+    stage: Stage = None
+
+@dataclass
+class DropTableRecord:
+    drop_table_type: str
+    selector: Selector
+    drop: Drop
+
+@dataclass
+class ItemBySource:
+    selector: str
+    resource_drop_chance: float
+    drop: Drop
+
+
+class DropTableProcessor:
+    pct_re = re.compile(r"\((?P<pct>.*)%\)$")
+
+    def __init__(
+            self,
+            url: str
+    ):
+        self.url = url
+        self.drop_table_records = []
+
 
     @staticmethod
-    def _split_on_blank_rows(rows: list[Tag]) -> list[list[Tag]]:
+    def _split_on_blank_rows(rows: list) -> list[list]:
         current_group = []
         for row in rows:
-            if 'blank-row' in row.get('class', []):
+            if 'blank-row' in row.get("class", []):
                 if current_group:
                     yield current_group
                     current_group = []
             else:
                 current_group.append(row)
 
-    @classmethod
-    def from_html_table(cls, t: Tag, header: str):
-        return cls(
-            name=header.rstrip(":"),
-            drop_tables=[DropTable.from_list_of_tags(d) for d in cls._split_on_blank_rows(t.select("tr"))]
-        )
+
+    def load_data(self):
+        resp = requests.get(self.url)
+        soup = BeautifulSoup(resp.text, "html.parser")
+        drop_tables = {}
+
+        for table in soup.select("table"):
+            header = table.find_previous_sibling("h3")
+            header_text = header.text.rstrip(":")
+            drop_tables[header_text] = []
+
+            # Break after hitting the sorties header, as the ItemsBySource is not yet implemented
+            if "sorties" in header_text.strip().lower():
+                break
+
+            # Iterate over the types
+            for drop_table in self._split_on_blank_rows(table.select("tr")):
+                # Each drop table is a list of rows
+                selector = None
+                drop_table_records = []
+                for row in drop_table:
+                    if th := row.select("th"):
+                        th = th[0]
+                        if th.attrs.get("colspan"):
+                            if "rotation" in th.text.strip().lower():
+                                rotation = th.text[-1]
+                                selector.rotation = rotation
+                            else:
+                                selector_text = th.text
+                                selector = Selector(
+                                    selector_text,
+                                    rotation=""
+                                )
+                    else:
+                        row_data = row.select("td")
+                        chance_text = row_data[-1].text
+                        drop_text = row_data[-2].text.strip()
+
+                        chance_desc, chance_amt = chance_text.rsplit("(", 1)
+                        chance_desc = chance_desc.strip()
+                        chance_amt = chance_amt[:-2]
+                        chance_amt = float(chance_amt)
+
+                        drop = Drop(
+                            drop_text,
+                            Chance(
+                                chance_desc,
+                                chance_amt
+                            )
+                        )
+
+                        drop_table_records.append(
+                            DropTableRecord(
+                                drop_table_type = header_text,
+                                selector = selector,
+                                drop = drop
+                            )
+                        )
+                drop_tables[header_text] = drop_tables[header_text] + drop_table_records
+
+
+        self.drop_table_records = drop_tables
 
 
 
